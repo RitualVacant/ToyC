75a07f567cf85e35f1f211ba645b4e277eb34579
innovation25 <3205568376@qq.com>
2022-02-27 01:07:01 +0800

    2022-2-27

--- build/CMakeFiles/MyScirpt.dir/DependInfo.cmake
+++ build/CMakeFiles/MyScirpt.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/lzj/code/program/scirpt/main.cpp" "/home/lzj/code/program/scirpt/build/CMakeFiles/MyScirpt.dir/main.cpp.o"
+  "/home/lzj/code/program/script/main.cpp" "/home/lzj/code/program/script/build/CMakeFiles/MyScirpt.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
